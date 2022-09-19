3a044c0f2569066d4e76b05861781f8748848e2e
blessed17 <68945307+blessed17@users.noreply.github.com>
2022-09-19 16:06:10 +0800

    Update config.py
    edit to my path

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
+customhost = "employee.cjj1z4ah4uho.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "khuweeming-bucket"
 customregion = "us-east-1"
