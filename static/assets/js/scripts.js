48d5432817457ea506975d796b9497f97e37dba3
TanHaoYou <111227874+TanHaoYou@users.noreply.github.com>
2022-09-22 17:21:00 +0800

    update js

---
+++ static/assets/js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2022 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+function showChanges(event){
+    if(event.target.files.length >0){
+        var src = URL.createObjectURL(event.target.files[0]);
+        var preview = document.getElementById("display_uploaded_img");
+        preview.src = src;
+        preview.style.display = "block";
+    }
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
